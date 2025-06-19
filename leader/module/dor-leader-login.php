1a8b2b3c08f24a0fb41383b1dd57bc0512880555
jblustre0125 <jblustre0025@gmail.com>
2025-06-19 09:50:16 +0800

    Done modifying the table layout

--- leader/module/dor-leader-login.php
+++ leader/module/dor-leader-login.php
@@ -13,11 +13,7 @@ $errorPrompt = "";
     <link rel="icon" type="image/png" href="../img/dor-1024.png">
     <link rel="stylesheet" href="../../css/bootstrap.min.css" />
     <link rel="stylesheet" href="../../css/index.css" />
-     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/bootstrap.bundle.min.js"></script>
     <link rel="manifest" href="../../manifest.json">
     <meta name="theme-color" content="#0d6efd">
     <meta name="mobile-web-app-capable" content="yes">
--- module/adm-mode.php
+++ module/adm-mode.php
@@ -27,13 +27,10 @@ if (isset($_GET['mode']) && $_GET['mode'] === 'operator') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? 'Default Title'; ?></title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+    <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../css/index.css" />
     <link href="../css/master.css" rel="stylesheet">
+    <script src="../../js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
