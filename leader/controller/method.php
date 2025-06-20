cdb19263a6fc015019577db36f68a48d0382ced8
jblustre0125 <jblustre0025@gmail.com>
2025-06-20 16:19:25 +0800

    Done updating the orientation of the tablet

--- leader/controller/method.php
+++ leader/controller/method.php
@@ -9,12 +9,72 @@
         }
 
         //get active hostnames
-        public function getActiveHostnames(){
-            $sql = "SELECT Hostname, IsActive FROM GenHostname WHERE IsActive = 1 ORDER BY Hostname";
-
-            return $this->db->execute($sql);
+        public function getActiveHostnames() {
+            $db = new DbOp(1);
+            $sql = "SELECT 
+                        h.HostnameId,
+                        h.Hostname,
+                        h.IsActive,
+                        d.RecordId
+                    FROM GenHostname h
+                    LEFT JOIN AtoDor d ON 
+                        d.HostnameId = h.HostnameId AND
+                        d.DorDate = CAST(GETDATE() AS DATE)
+                    WHERE h.IsActive = 1
+                    ORDER BY h.Hostname";
+            return $db->execute($sql);
         }
 
+        // In your Method class
+        public function getOnlineTablets($excludeHostnameId = null) {
+            $query = "SELECT 
+                        HostnameId as RecordId, 
+                        Hostname, 
+                        IsActive,
+                        IsLoggedin
+                    FROM GenHostname 
+                    WHERE IsLoggedin = 1";
+            
+            if ($excludeHostnameId) {
+                $query .= " AND HostnameId != ?";
+                return $this->db->execute($query, [$excludeHostnameId]);
+            }
+            
+            return $this->db->execute($query);
+        }
+        public function getTabletStatus() {
+            $query = "SELECT 
+                        HostnameId as RecordId, 
+                        Hostname, 
+                        IsActive,
+                        IsLoggedin
+                    FROM GenHostname 
+                    ORDER BY IsLogin DESC, Hostname ASC";
+            return $this->db->execute($query);
+        }
+
+        public function getAllHostnames() {
+            $query = "SELECT HostnameId as RecordId, Hostname, IsActive 
+                    FROM GenHostname 
+                    ORDER BY IsActive DESC, Hostname ASC";
+            return $this->db->execute($query);
+        }
+        
+        public function getCurrentTablet($hostnameId) {
+            $hostnames = $this->getActiveHostnames();
+            foreach ($hostnames as $hostname) {
+                if ($hostname['HostnameId'] == $hostnameId) {
+                    return $hostname;
+                }
+            }
+            return null;
+        }
+
+        public function updateTabletStatus($hostnameId, $status) {
+            $sql = "EXEC UpdGenHostname @HostnameId=?, @IsLoggedIn=?";
+            $this->db->execute($sql, [$hostnameId, $status]);
+            return true;
+        }
         //get visual checkpoints by DorTypeId
         public function getVisualCheckpoint($dorTypeId){
             $sql = "SELECT * FROM GenDorCheckpointVisual WHERE DorTypeId = ? AND IsActive = 1 ORDER BY SequenceId ";
